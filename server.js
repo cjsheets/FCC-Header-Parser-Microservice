b798c7e730a48aeaceb25d438594ba5d5f6bba6d
Chad Sheets <cjsheets@gmail.com>
2016-11-06 16:24:15 +0000

    added header parsing

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require("http");  
 const url = require('url');
+const UAParser = require('ua-parser-js');
 
+var parser = new UAParser();
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  for( item of res.headers ){
-    console.log(item);
-  }
+  parser.setUA(req.headers["user-agent"]);
+  var result = parser.getResult(),
+    language = req.headers["accept-language"].split(',');
+  res.end("{\"ipaddress\":\""+req.headers["x-forwarded-for"]+"\",\"language\":\""+language[0]+
+    "\",\"software\":\""+result.os.name+" "+result.cpu.architecture+" "+result.engine.name+" "+result.engine.version+"\"}")
 }).listen(process.env.PORT || 8080);
